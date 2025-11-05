d16192fd103dd0aa6952dd0e303e51100107c068
10555gff <1073245193@qq.com>
2025-11-06 03:31:47 +0800

    q

--- node-ble-demo/preload2.js
+++ node-ble-demo/preload2.js
@@ -33,9 +33,9 @@ const TARGET_NAME = 'QY-QYSC-S-D2D3';
 
 
 noble.on('discover', async (peripheral)=> {
-    console.log('发现设备，名称：', peripheral.advertisement.localName);
+    const deviceName= `${peripheral.advertisement.localName || "未知设备"} (${peripheral.address})`;
+    console.log('发现设备，名称：', deviceName);
     if (TARGET_NAME === peripheral.advertisement.localName) {
-      //targetDevice = peripheral;
       await noble.stopScanningAsync();
       console.log('找到目标设备，停止扫描，准备连接...');
 
@@ -43,32 +43,33 @@ noble.on('discover', async (peripheral)=> {
       try {
           console.log("开始连接");
           await peripheral.connectAsync();
-          // 发现指定服务和特征
-          const { services, characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(
-            [SERVICE_UUID],
-            [CHRCT_UUID_CUBE]
-          );
-          
+
+          // // 发现指定服务和特征
+          // const { services, characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(
+          //   [SERVICE_UUID],
+          //   [CHRCT_UUID_CUBE]
+          // );
+
+          const services=await peripheral.discoverServicesAsync([SERVICE_UUID]);
+          const characteristics=await services[0].discoverCharacteristicsAsync([CHRCT_UUID_CUBE]);
+
 
           console.log('找到服务:', services[0]);
-
-
-
-          cubeChar = characteristics[0];
+          const cubeChar = characteristics[0];
           console.log('找到特征:', cubeChar);
 
-                    // ========== 订阅通知（noble 方式）==========
-          if (cubeChar.properties.includes('notify') || cubeChar.properties.includes('indicate')) {
-            cubeChar.on('data', (data, isNotification) => {
-              console.log('收到数据:', data.toString('hex'));
-              onCubeEvent(data); // 你的处理函数
-            });
+          //           // ========== 订阅通知（noble 方式）==========
+          // if (cubeChar.properties.includes('notify') || cubeChar.properties.includes('indicate')) {
+          //   cubeChar.on('data', (data, isNotification) => {
+          //     console.log('收到数据:', data.toString('hex'));
+          //     onCubeEvent(data); // 你的处理函数
+          //   });
 
-            await cubeChar.subscribeAsync();
-            console.log('已订阅通知');
-          } else {
-            console.log('特征不支持通知');
-          }
+          //   await cubeChar.subscribeAsync();
+          //   console.log('已订阅通知');
+          // } else {
+          //   console.log('特征不支持通知');
+          // }
 
 
 
